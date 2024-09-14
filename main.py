46a54e6e71f9e4a4b1c133b4fc23c9267a7cfb83
Lars de Kooter <de.kooter.lars@gmail.com>
2024-09-14 09:57:21 +0200

    Add printing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 
 env = gym.make("CartPole-v1", render_mode="human")
 BATCHSIZE = 100
+GAMMA = 0.99
 
 state, _ = env.reset()
 
@@ -32,6 +33,9 @@ class Network:
         self.memory = deque(maxlen=100_000)
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
+        self.ai = 0
+        self.rand = 0
+        self.rewardPerGame = []
 
     def train(self, state, newState, action, reward, done):
         # Add experience to memory
@@ -59,7 +63,7 @@ class Network:
         nextQValues = self.targetModel(newStates).max(1)[0].detach()
 
         # Compute target Q values
-        targetQValues = rewards + (1 - dones) * 0.99 * nextQValues
+        targetQValues = rewards + (1 - dones) * GAMMA * nextQValues
 
         # Compute loss
         loss = self.criterion(currentQValues, targetQValues)
@@ -79,8 +83,10 @@ class Network:
         e = 0.0001 + 0.9 * np.exp(1e-6 * -self.steps)
         self.steps += 1
         if np.random.random() < e:
+            self.rand += 1
             return env.action_space.sample()
         else:
+            self.ai += 1
             with torch.no_grad():
                 return (
                     self.model(torch.tensor(state, dtype=torch.float32)).argmax().item()
@@ -93,7 +99,13 @@ while True:
     action = network.getAction(state)
     nextState, reward, terminated, truncated, info = env.step(action)
     network.train(state, nextState, action, 0 if terminated else reward, terminated)
+    network.rewardPerGame[ngames] += reward
 
     if truncated or terminated:
         ngames += 1
+        print(
+            f"Games: {ngames}, AI: {network.ai}, Random: {network.rand}, Percentage: {round(network.ai / (network.ai + network.rand) * 100.0)}, Reward: {network.rewardPerGame[ngames]}"
+        )
+        network.ai = 0
+        network.rand = 0
         state, info = env.reset()
