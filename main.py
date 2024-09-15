75cd3a6b83f2d59d2da687c8aa76208ee44f714d
Lars de Kooter <de.kooter.lars@gmail.com>
2024-09-15 10:39:36 +0200

    Fast pretraining

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import torch.optim as optim
 from collections import deque
 import random
 import numpy as np
+from tqdm import tqdm
 
 
-env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make("CartPole-v1")
+env.metadata["render_fps"] = 1000000
 BATCHSIZE = 100
 GAMMA = 0.99
+EPISODES = 1000000
 
 state, _ = env.reset()
 
@@ -95,13 +98,34 @@ class Network:
 
 ngames = 0
 network = Network()
+for i in tqdm(range(EPISODES)):
+    while True:
+        action = network.getAction(state)
+        nextState, reward, terminated, truncated, info = env.step(action)
+        network.train(state, nextState, action, 0 if terminated else reward, terminated)
+        network.rewardPerGame[ngames] += reward
+        state = nextState
+
+        if truncated or terminated:
+            # print(
+            #     f"Games: {ngames}, AI: {network.ai}, Random: {network.rand}, Percentage: {round(network.ai / (network.ai + network.rand) * 100.0)}, Reward: {int(network.rewardPerGame[ngames])}, Steps: {network.steps}, Epsilon: {round(network.epsilon, 3)}"
+            # )
+            ngames += 1
+            network.ai = 0
+            network.rand = 0
+            network.rewardPerGame.append(0)
+            state, info = env.reset()
+            break
+
+env.close()
+env = gym.make("CartPole-v1", render_mode="human")
+state, info = env.reset()
 while True:
     action = network.getAction(state)
     nextState, reward, terminated, truncated, info = env.step(action)
     network.train(state, nextState, action, 0 if terminated else reward, terminated)
     network.rewardPerGame[ngames] += reward
     state = nextState
-
     if truncated or terminated:
         print(
             f"Games: {ngames}, AI: {network.ai}, Random: {network.rand}, Percentage: {round(network.ai / (network.ai + network.rand) * 100.0)}, Reward: {int(network.rewardPerGame[ngames])}, Steps: {network.steps}, Epsilon: {round(network.epsilon, 3)}"
